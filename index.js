d681c7f1c3ca2476cd1407d07ae19d2bca5d2bec
Kelvin <frednjogu22@gmail.com>
2024-03-30 23:47:58 +0300

    feat scope

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution in this file!
+//Define the initial values for the variables
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'tom'
+
+// Function to uppercase the customerName
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+// Set the best customer to 'not bob'
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+//Call setBestCustomer to set the best customer
+setBestCustomer();
+console.log(bestCustomer);
+
+
+// Overwrite the best customer to 'maybe bob'
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+overwriteBestCustomer();
+console.log(bestCustomer);
+
+// Reset bestCustomer to undefined
+bestCustomer = undefined;
+
+console.log(bestCustomer); 
+
+// Attempt to change the least favorite customer 
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'don';
+}
+changeLeastFavoriteCustomer();
+console.log(leastFavoriteCustomer);
